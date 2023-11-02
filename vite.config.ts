58ad455f237f3e05d3506b5e1470a91c5dd678b2
夏彬阳 <xby020@163.com>
2023-11-02 14:47:51 +0800

    fix(package): Fix glitch with new version about package.

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,19 @@
-import vue from '@vitejs/plugin-vue';
-import { resolve } from 'path';
-import WindiCSS from 'vite-plugin-windicss';
-import Components from 'unplugin-vue-components/vite';
-import Icons from 'unplugin-icons/vite';
-import IconsResolver from 'unplugin-icons/resolver';
-import AutoImport from 'unplugin-auto-import/vite';
-import { UserConfigExport, ConfigEnv, loadEnv } from 'vite';
-import { configMockPlugin } from './src/utils/mock';
-import { NaiveUiResolver } from 'unplugin-vue-components/resolvers';
-import { createHtmlPlugin } from 'vite-plugin-html';
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+import WindiCSS from 'vite-plugin-windicss'
+import Components from 'unplugin-vue-components/vite'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+import AutoImport from 'unplugin-auto-import/vite'
+import { UserConfigExport, ConfigEnv, loadEnv } from 'vite'
+import { configMockPlugin } from './src/utils/mock'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 // https://vitejs.dev/config/
 export default ({ mode }: ConfigEnv): UserConfigExport => {
-  const root = process.cwd();
-  const env = loadEnv(mode, root);
+  const root = process.cwd()
+  const env = loadEnv(mode, root)
 
   return {
     plugins: [
@@ -27,16 +27,16 @@ export default ({ mode }: ConfigEnv): UserConfigExport => {
         inject: {
           data: {
             title: env.VITE_APP_TITLE,
-            version: env.VITE_APP_VERSION
-          }
-        }
+            version: env.VITE_APP_VERSION,
+          },
+        },
       }),
 
       // auto import components in templates
       Components({
         dts: 'src/types/components.d.ts',
         dirs: ['src/components'],
-        resolvers: [IconsResolver(), NaiveUiResolver()]
+        resolvers: [IconsResolver(), NaiveUiResolver()],
       }),
 
       // auto import icons
@@ -46,60 +46,61 @@ export default ({ mode }: ConfigEnv): UserConfigExport => {
       AutoImport({
         dts: 'src/types/auto-imports.d.ts',
         eslintrc: {
-          enabled: true
+          enabled: true,
         },
         imports: [
           // presets
           'vue',
           'vue-router',
           'pinia',
-          '@vueuse/core'
+          '@vueuse/core',
         ],
         resolvers: [
           IconsResolver({
-            prefix: 'icon'
-          })
-        ]
+            prefix: 'icon',
+          }),
+        ],
       }),
 
       // mock plugin
-      configMockPlugin(env)
+      configMockPlugin(env),
     ],
     resolve: {
       alias: {
         '@': resolve(__dirname, './src'),
-        '@pages': resolve(__dirname, './src/pages')
-      }
+        '@pages': resolve(__dirname, './src/pages'),
+      },
     },
     json: {
       // see https://cn.vitejs.dev/config/shared-options.html#json-stringify
-      stringify: true
+      stringify: true,
     },
     preview: {
-      open: true
+      open: true,
     },
     server: {
+      host: true,
       port: Number(env.VITE_PORT) || 14514,
       proxy: {
         '/api/v1': {
           target: env.VITE_API_URL,
           changeOrigin: true,
           ws: true,
-          rewrite: (path) => path.replace(new RegExp(`^/api/v1`), '')
+          rewrite: (path) => path.replace(new RegExp(`^/api/v1`), ''),
         },
         '/upload': {
           target: env.VITE_UPLOAD_URL,
           changeOrigin: true,
           ws: true,
-          rewrite: (path) => path.replace(new RegExp(`^/upload`), '')
+          rewrite: (path) => path.replace(new RegExp(`^/upload`), ''),
         },
         '/media': {
           target: env.VITE_MEDIA_URL,
           changeOrigin: true,
           ws: true,
-          rewrite: (path) => path.replace(new RegExp(`^/media`), '')
-        }
-      }
-    }
-  };
-};
+          rewrite: (path) => path.replace(new RegExp(`^/media`), ''),
+        },
+      },
+    },
+  }
+}
